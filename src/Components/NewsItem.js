f58c111f93f96e2e9fc31cfdf1e858eebd5e2d1b
Aaryan-02 <102778679+Aaryan-02@users.noreply.github.com>
2023-06-02 19:45:49 +0530

    Add files via upload

---
+++ src/Components/NewsItem.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const NewsItem = (props) => {
+
+    let { title, description, imageUrl, newsUrl, author, date, source } = props;
+    return (
+        <div className='my-3'>
+
+            <div className="card">
+                <div style={{ display: "flex", justifyContent: "flex-end", position: "absolute", right: "0" }}>
+                    <span className='badge rounded-pill' style={{ backgroundColor: "orangered" }}>{source}</span>
+                </div>
+                <img src={!imageUrl ? "https://www.hindustantimes.com/ht-img/img/2023/02/07/1600x900/Bard_1675741184622_1675741192605_1675741192605.png" : imageUrl} className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">{title}</h5>
+                    <p className="card-text">{description}...</p>
+                    <p className="card-text"><small className="text-muted">By {!author ? "Unknown" : author} on {new Date(date).toGMTString()}</small></p>
+                    <div className="rd">
+                        <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark readBtn">Read more</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default NewsItem
